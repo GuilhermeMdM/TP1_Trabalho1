4f48e4d730ec34838fe6aad3ee6068aca8e32f7d
caiocesar <caiocesar20032003@gmail.com>
2023-10-08 21:32:04 -0300

    adicionei o header

--- dominios.cpp
+++ dominios.cpp
@@ -1,3 +1,6 @@
+#include "dominios.hpp"
+
+
 void Senha::validar(string senha){
 char minusculas[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 char maiusculas[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
