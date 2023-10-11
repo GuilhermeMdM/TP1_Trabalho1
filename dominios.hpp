ec630a6250827bc1c7269d4fc655339ecfe75bf3
caiocesar <caiocesar20032003@gmail.com>
2023-10-11 20:15:15 -0300

    declaração de Texto e ChecaCaractere

--- dominios.hpp
+++ dominios.hpp
@@ -1,12 +1,41 @@
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
-   class Senha{
-private:
-    string senha;
-    void validar(string);
-public:
-    void setValor(string);
-    string getValor();
-   };
+class Senha {
+    private:
+        string senha;
+        void validar(string);
+    public:
+        void setValor(string);
+        string getValor();
+};
+
+class Texto {
+    private:
+        string texto;
+        void validar (string);
+    public:
+        void setTexto(string);
+        string getTexto();
+};
+
+class ChecaCaractere{
+    public:
+
+        static int caractere_ascii;
+        static char sinais_de_pontuacao[5];
+
+        static bool e_maiuscula_sem_acento(char);
+
+        static bool e_minuscula_sem_acento(char);
+
+        static bool e_pontuacao(char);
+
+        static bool e_digito (char);
+
+        static bool e_embranco (char);
+};
+
+
