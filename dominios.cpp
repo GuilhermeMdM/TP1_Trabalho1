4632b3dd8bdff9fc02e8356f7e1d837640e5a40c
caiocesar <caiocesar20032003@gmail.com>
2023-10-14 09:37:02 -0300

    refactor: cpplint refactor

--- dominios.cpp
+++ dominios.cpp
@@ -1,30 +1,32 @@
 #include "dominios.hpp"
 #include <vector>
 
-//////Métodos da classe Senha///////
+////// Métodos da classe Senha ///////
 
 void Senha::validar(string senha) {
-    char minusculas[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    char maiusculas[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    char numeros[10]={'0','1','2','3','4','5','6','7','8','9'};
-    char especiais[5]={'.',',',';','?','!'};
+    char minusculas[26] = {'a','b','c','d','e','f','g','h','i','j','k',
+    'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char maiusculas[26] = {'A','B','C','D','E','F','G','H','I','J','K',
+    'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char numeros[10] = {'0','1','2','3','4','5','6','7','8','9'};
+    char especiais[5] = {'.',',',';','?','!'};
     bool temMinuscula = false;
     bool temMaiuscula = false;
     bool temNumero = false;
     bool temEspecial = false;
 
-    if (senha.length()!=5) {
+    if (senha.length() != 5) {
         throw invalid_argument("Valor invalido");
     }
     // Verificação de Caracteres Repetidos.
     int CurrPos = 0;
     int incremento;
     int PosComparada;
-    while(CurrPos<4) {
+    while (CurrPos < 4) {
         incremento = 1;
         PosComparada = CurrPos + incremento;
-        while (PosComparada<=4) {
-            if(senha[CurrPos] == senha[PosComparada]){
+        while (PosComparada <= 4) {
+            if (senha[CurrPos] == senha[PosComparada]) {
                 throw invalid_argument("Valor invalido");
             }
             incremento++;
@@ -32,7 +34,7 @@ void Senha::validar(string senha) {
         CurrPos++;
     }
 
-    for(char c1 : senha) {
+    for (char c1 : senha) {
         for (char c2 : minusculas) {
             if (c1 == c2) {
                 temMinuscula = true;
@@ -78,7 +80,7 @@ int ChecaCaractere::caractere_ascii = 0;
 char ChecaCaractere::sinais_de_pontuacao[5] = {';', ',', '.', '!', '?'};
 
 bool ChecaCaractere::e_maiuscula_sem_acento(char caractere) {
-    caractere_ascii = int(caractere);
+    caractere_ascii = static_cast<int>(caractere);
     if (caractere_ascii < 65 || caractere_ascii > 90) {
         return false;
     }
@@ -86,8 +88,8 @@ bool ChecaCaractere::e_maiuscula_sem_acento(char caractere) {
 }
 
 bool ChecaCaractere::e_minuscula_sem_acento(char caractere) {
-    caractere_ascii = int(caractere);
-    if (caractere_ascii < 97 || caractere_ascii > 122){
+    caractere_ascii = static_cast<int>(caractere);
+    if (caractere_ascii < 97 || caractere_ascii > 122) {
         return false;
     }
     return true;
@@ -103,7 +105,7 @@ bool ChecaCaractere::e_pontuacao(char caractere) {
 }
 
 bool ChecaCaractere::e_digito(char caractere) {
-    caractere_ascii = int(caractere);
+    caractere_ascii = static_cast<int>(caractere);
     if (caractere_ascii < 48 || caractere_ascii > 57) {
         return false;
     }
@@ -144,12 +146,14 @@ void Texto::validar(string texto) {
         } else if (ChecaCaractere::e_pontuacao(Char_Atual)) {
             FormatoValido = true;
             // Verificação de espaços em sequência.
-            if (currPos < texto.length() - 1) {  // Não faz sentido checar o último caractere.
+            // Não faz sentido checar a última posição.
+            if (currPos < texto.length() - 1) {
                 char ProximoCaractere = texto[currPos + 1];
                 if (ChecaCaractere::e_pontuacao(ProximoCaractere)) {
                     throw invalid_argument("Pontuação em Seguida");
                 }
-                if (Char_Atual != ',' && Char_Atual != ';' && !ChecaCaractere::e_maiuscula_sem_acento(ProximoCaractere)) {
+                if (Char_Atual != ',' && Char_Atual != ';' &&
+                !ChecaCaractere::e_maiuscula_sem_acento(ProximoCaractere)) {
                     throw invalid_argument("Combinação inválida");
                 }
             }
@@ -174,7 +178,7 @@ void Texto::setTexto(string texto) {
     this->texto = texto;
 }
 
-//////Métodos da classe Limite///////
+////// Métodos da classe Limite ///////
 
 void Limite::validar(int limite) {
     if (limite != LIMITE && limite != 2*LIMITE && limite != 3*LIMITE
@@ -188,7 +192,7 @@ void Limite::setValor(int limite) {
     this->limite = limite;
 }
 
-//////Métodos da classe Coluna///////
+////// Métodos da classe Coluna ///////
 
 void Coluna::validar(string coluna) {
     if (coluna != "SOLICITADO" && coluna != "EM EXECUCAO" &&
@@ -202,7 +206,7 @@ void Coluna::setValor(string coluna) {
     this->coluna = coluna;
 }
 
-//////Métodos da classe Codigo///////
+////// Métodos da classe Codigo ///////
 
 void Codigo::validar(string codigo) {
     if (codigo.length() != 4) {
