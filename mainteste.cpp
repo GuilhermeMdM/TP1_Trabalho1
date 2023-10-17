20136b8cc84cc139431276ff792be4a8f3c1ce9e
ogmoises <144768841+ogmoises@users.noreply.github.com>
2023-10-17 13:46:10 -0300

    Update mainteste.cpp
    Modificada mainteste.cpp

--- mainteste.cpp
+++ mainteste.cpp
@@ -4,6 +4,7 @@
 
 int main(){
     cout << "TESTE DE DOMINIOS" << endl;
+
     TUTexto testeTexto;
     switch(testeTexto.run()) {
     case TUTexto::SUCESSO: cout << "SUCESSO - TEXTO" << endl;
@@ -11,5 +12,22 @@ int main(){
     case TUTexto::FALHA  : cout << "FALHA   - CODIGO" << endl;
                             break;
     }
+
+    TUEmail testeEmail;
+    switch(testeEmail.run()) {
+    case TUEmail::SUCESSO: cout << "SUCESSO - EMAIL" << endl;
+                            break;
+    case TUEmail::FALHA  : cout << "FALHA   - EMAIL" << endl;
+                            break;
+    }
+
+    TUSenha testeSenha;
+    switch(testeSenha.run()) {
+    case TUSenha::SUCESSO: cout << "SUCESSO - SENHA" << endl;
+                            break;
+    case TUSenha::FALHA  : cout << "FALHA   - SENHA" << endl;
+                            break;
+    }
+    
     return 0;
 }
