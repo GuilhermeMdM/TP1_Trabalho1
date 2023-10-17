3e920cdcba730a07b746af5c01c673c323629143
ogmoises <144768841+ogmoises@users.noreply.github.com>
2023-10-17 13:45:10 -0300

    Update testa_dominios.cpp
    Adicionados métodos de TUEmail e TUSenha

--- testa_dominios.cpp
+++ testa_dominios.cpp
@@ -1,6 +1,6 @@
 #include "testa_dominios.hpp"
 
-///// M�todos de TUTexto /////
+///// Métodos de TUTexto /////
 
 const string TUTexto::VALOR_VALIDO   = "Teste valido 1!Ok ";
 const string TUTexto::VALOR_INVALIDO = "diosanio9  jdsidadw!*";
@@ -37,6 +37,94 @@ void TUTexto::testarCenarioFalha(){
 }
 
 int TUTexto::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
+///// Métodos de TUEmail /////
+
+const string TUEmail::VALOR_VALIDO   = "trabalho1@dominioemail.com";
+const string TUEmail::VALOR_INVALIDO = "invalido.@email.com";
+
+void TUEmail::setUp() {
+    email = new Email();
+    estado = SUCESSO;
+}
+
+void TUEmail::tearDown(){
+    delete email;
+}
+
+void TUEmail::testarCenarioSucesso() {
+    try {
+        email->setEmail(VALOR_VALIDO);
+        if (email->getEmail() != VALOR_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA;
+    }
+}
+
+void TUEmail::testarCenarioFalha(){
+    try{
+        email->setEmail(VALOR_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        if (email->getEmail() == VALOR_INVALIDO)
+            estado = FALHA;
+    }
+}
+
+int TUEmail::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
+///// Métodos de TUSenha /////
+
+const string TUSenha::VALOR_VALIDO   = "Aa1.p";
+const string TUSenha::VALOR_INVALIDO = "B3a1j";
+
+void TUSenha::setUp() {
+    senha = new Senha();
+    estado = SUCESSO;
+}
+
+void TUSenha::tearDown(){
+    delete senha;
+}
+
+void TUSenha::testarCenarioSucesso() {
+    try {
+        senha->setSenha(VALOR_VALIDO);
+        if (senha->getSenha() != VALOR_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA;
+    }
+}
+
+void TUSenha::testarCenarioFalha(){
+    try{
+        senha->setSenha(VALOR_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        if (senha->getSenha() == VALOR_INVALIDO)
+            estado = FALHA;
+    }
+}
+
+int TUSenha::run(){
     setUp();
     testarCenarioSucesso();
     testarCenarioFalha();
